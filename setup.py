5b40e7a2f10e864173970c618287fc41c0f8e34a
Shashank Raj <shashank.raj2009@gmail.com>
2020-07-17 03:24:18 +0530

    Added Gaussian, General and Binomial Distributions

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='all_stats_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['all_stats_distributions'],
+      author='Shashank Raj',
+      author_email='shashank.raj2009@gmail.com',
+      zip_safe=False)
